9e46bf4fcefec91f861d9e3e0d43af19598d0c7c
xcplus <golovesx@gmail.com>
2023-07-02 22:38:17 +0800

    错误处理

--- 4.http/11.静态资源服务/server.js
+++ 4.http/11.静态资源服务/server.js
@@ -7,19 +7,67 @@
 
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
+
+const mimes = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/javascript',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  mp4: 'video/mp4',
+  mp3: 'audio/mpeg',
+  json: 'application/json'
+}
 
 const server = http.createServer((request, response) => {
+  if(request.method !== 'GET') {
+    response.statusCode = 405
+    response.end('<h1>405 Method Not Allowed</h1>')
+    return
+  }
   // 获取请求的URL路径
   let { pathname } = new URL(request.url, 'http://localhost:9000')
+  // 声明一个变量
+  let root = __dirname + '/page'
   // 拼接文件路径
-  let filePath = __dirname + '/page' + pathname
+  let filePath = root + pathname
   // 读取文件 fs 异步 API
   fs.readFile(filePath, (err, data) => {
     if(err) {
-      response.statusCode = 404
-      response.end('Not found')
+      // 设置字符集
+      response.setHeader('content-type', 'text/html;charset=utf-8')
+      // 判断错误代码
+      switch (err.code) {
+        case 'ENOENT':
+          response.statusCode = 404
+          response.end('<h1>404 Not Found</h1>')
+        case 'EPERM':
+          response.statusCode = 403
+          response.end('<h1>403 Forbidden</h1>')
+        default:
+          response.statusCode = 500
+          response.end('<h1>Internal Server Error</h1>')
+      }
       return 
     } 
+    // 获取文件的后缀名
+    let ext = path.extname(filePath).slice(1)
+    console.log("文件后缀名: ", ext);
+    // 获取对应的类型
+    let type = mimes[ext]
+    if(type) {
+      // 匹配到了
+      if(ext === 'html') {
+        response.setHeader('content-type', type + ';charset=utf-8')
+      } else {
+        response.setHeader('content-type', type)
+      }
+    } else {
+      // 没有匹配到
+      response.setHeader('content-type', 'application/octet-stream')
+    }
     response.end(data)
   })
 })
--- README.md
+++ README.md
@@ -30,3 +30,31 @@
 3xx             重定向消息
 4xx             客户端错误响应
 5xx             服务端错误响应
+
+
+###  资源类型(mime类型)
+用来表示文档、文字或字节流的性质和格式。
+```js
+// mime 类型结构: [type]/[subtype]
+// 例如: text/html text/css image/jpeg image/png application/json
+```
+http服务可以设置响应头Content-Type来表明响应体的MIME类型。
+下面是常见文件对应的mime类型:
+```js
+const mimes = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/javascript',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  mp4: 'video/mp4',
+  mp3: 'audio/mpeg',
+  json: 'application/json'
+}
+```
+
+```js
+// 对于未知的资源类型，可以选择application/octet-stream类型, 浏览器在遇到该类型的响应时，会对响应体
+// 内容进行独立存储，也就是我们常见的下载效果
+```
