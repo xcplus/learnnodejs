34a144e4b69617d706d112b3f45a6ed0a2013d51
xcplus <golovesx@gmail.com>
2023-07-15 12:20:51 +0800

    处理文件上传

--- 7.express/learnexpress/15.generator/routes/index.js
+++ 7.express/learnexpress/15.generator/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const formidable = require('formidable')
 var router = express.Router();
 
 /* GET home page. */
@@ -13,7 +14,26 @@ router.get('/portrait', (req, res) => {
 
 // 处理文件上传
 router.post('/portrait', (req, res) => {
-  res.send('上传成功')
+  // 创建表单对象
+  const form = new formidable.IncomingForm({
+    multiples: true,
+    // 保存目录
+    uploadDir: __dirname + '/../public/images',
+    // 保持文件后缀
+    keepExtensions: true
+  });
+
+  // 解析请求报文
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log('fields: ', fields);
+    console.log('files: ', files);
+    let url = '/images/' + files.avatar[0].newFilename
+    res.send(url)
+  });
 })
 
 module.exports = router;
